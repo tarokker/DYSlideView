470ddbbdca6279ef853d98cb2ab70fd809143d5e
sattar.falahati <sattar.falahati@gmail.com>
2017-09-14 16:56:08 +0200

    Change the selected button behavior

--- Class/DYSlideView.m
+++ Class/DYSlideView.m
@@ -224,25 +224,23 @@
 
 - (void)selectButtonWithIndex:(NSInteger)newIndex
 {
-    if ( _initializating )
-    {
+    if ( _initializating ) {
         _indexForDefaultItem = [NSNumber numberWithInteger:newIndex];
     }
-    else
-    {
-        for ( UIButton *mButton in _slideBarButtons )
-        {
-            if ( mButton.tag == newIndex )
-            {
-                _selectedButton = mButton;
-                [self setNeedsLayout];
-                [self layoutIfNeeded];
-                [self _updateSelectedButton:[_slideBarButtons objectAtIndex:newIndex]];
+    else {
+        for ( UIButton *mButton in _slideBarButtons ) {
+            if ( mButton.tag == newIndex ) {
+                [self buttonClicked:mButton];
+//                _selectedButton = mButton;
+//                [self setNeedsLayout];
+//                [self layoutIfNeeded];
                 return;
             }
         }
     }
 }
+
+
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
